621b68971c19665fa589cecbadc2b2e08f735d5b
Aleceason199 <aleceason@hotmail.com>
2023-06-05 16:30:55 +1000

    Finished all functionality, still need to change html and css to look better but other than that its done

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 //list of selections for the getComputerChoice function
-var selections = ["ROCK", "PAPER", "SCISSORS"];
+var selections = ["ROCK", "SCISSORS", "PAPER"];
+var selections_ = ["PAPER", "ROCK", "SCISSORS", "PAPER", "ROCK"]
+
 var playerCount = 0;
 var computerCount = 0;
 var result;
+
+//gets the game div
+const gameDiv = document.querySelector("#gameDiv");
 
 //getting the score
 const playerCounter = document.querySelector("#playerScore");
 const computerCounter = document.querySelector("#computerScore");
 
+//gets message elements
 const choiceMessage = document.querySelector("#choiceMessage")
 const winMessage = document.querySelector("#winMessage")
 
@@ -23,6 +29,17 @@ gameButtons.forEach((button) => {
     });
  });
 
+ //creates a go again button to be added once the game is over
+const messageDiv = document.querySelector("#messageBox");
+
+const againButton = document.createElement('button');
+againButton.innerHTML = "GO AGAIN?"
+againButton.id = "againButton"
+againButton.addEventListener('click', () => {goAgain()});
+
+
+
+
 
 
 //function that returns a random selection from the selections array
@@ -33,30 +50,23 @@ function getComputerChoice(){
 
 //Function that takes the inputs from both players and returns if the player wins or loses
 function playRound(playerChoice, computerChoice){
-    //prints the choices
+
+    //shows the messages on page
     choiceMessage.innerHTML = ("YOU CHOSE: " + playerChoice + "<br>COMPUTER CHOSE: " + computerChoice)
 
-    //checks who won the game
-    if(playerChoice === computerChoice){
-        result = "draw";
+    //gets the players selections index and adds 1 to match it to the selections_ array
+    playerIndex = selections.indexOf(playerChoice) + 1;
+
+    //checks if the selection is above or below the computers selection in the selections array_
+    //returns the result of the play
+    if (selections_[playerIndex+1] === computerChoice){
+        result = "win"
     }
-    else if(playerChoice === "ROCK"){
-        if(computerChoice==="PAPER")
-            result = "lose";
-        if(computerChoice==="SCISSORS")
-            result = "win";
+    else if(selections_[playerIndex-1] === computerChoice){
+        result = "lose"
     }
-    else if (playerChoice === "PAPER"){
-        if(computerChoice==="SCISSORS")
-            result = "lose";
-        if(computerChoice==="ROCK")
-            result = "win";
-    }
-    else if (playerChoice === "SCISSORS"){
-        if(computerChoice==="ROCK")
-            result = "lose";
-        if(computerChoice==="PAPER")
-            result = "win";
+    else{
+        result = "draw"
     }
 
     //returns the winner and changes the player count
@@ -78,15 +88,51 @@ function playRound(playerChoice, computerChoice){
     playerCounter.innerHTML = playerCount;
     computerCounter.innerHTML = computerCount;
 
-    //responds based on who wins
+    //calls the game end function
     if(playerCount === 3){
-        console.log("CONGRATS YOU WON!!")
+        gameEnd("player")
     }
     else if(computerCount === 3)
     {
-        console.log("you lost lol hahah")
+        gameEnd("computer")
+    }
+}
+
+//function to be called when the game ends takes a winner variable
+function gameEnd(winner){
+
+    //disables the game buttons
+    gameButtons.forEach((button) => {
+        button.disabled = true;
+    });
+    
+    // depending on the inputed winner displays a message
+    if(winner==="player"){
+        winMessage.innerHTML = "YOU WON THE GAME!<br>WOULD YOU LIKE TO GO AGAIN?"
+    }
+    else{
+        winMessage.innerHTML = "YOU LOST THE GAME!<br>WOULD YOU LIKE TO GO AGAIN?"
     }
 
-}   
+    //creates a temporary go again button
+    tempButton = messageDiv.appendChild(againButton);
 
+}
+//function to be called when the go again button is clicked
+function goAgain(){
+
+    //enables the game buttons again
+    gameButtons.forEach((button) => {
+        button.disabled = false;
+    });
+
+    //resets all the elements on the page back to the orignal values
+    tempButton.remove()
+    winMessage.innerHTML = ""
+    choiceMessage.innerHTML = ""
+    playerCount = 0
+    computerCount = 0
+    playerCounter.innerHTML = playerCount;
+    computerCounter.innerHTML = computerCount;
+}
 
