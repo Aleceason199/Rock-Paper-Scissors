482140422c207d2c2cb83bc25fce28d6adbfda38
Aleceason199 <aleceason@hotmail.com>
2023-06-05 14:44:55 +1000

    created basic ui functionality

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 var selections = ["ROCK", "PAPER", "SCISSORS"];
 var playerCount = 0;
 var computerCount = 0;
+var result;
+
+//getting the score
+const playerCounter = document.querySelector("#playerScore");
+const computerCounter = document.querySelector("#computerScore");
+
+const choiceMessage = document.querySelector("#choiceMessage")
+const winMessage = document.querySelector("#winMessage")
+
+//Gets the buttons from the choices category
+const choiceButtons = document.querySelector("#choices")
+const gameButtons = choiceButtons.querySelectorAll("button")
+
+//for loop for each of the buttons and adds event listeners to them which trigger playRound()
+gameButtons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+        playRound(button.id, getComputerChoice());
+    });
+ });
+
+
 
 //function that returns a random selection from the selections array
 function getComputerChoice(){
@@ -12,7 +34,7 @@ function getComputerChoice(){
 //Function that takes the inputs from both players and returns if the player wins or loses
 function playRound(playerChoice, computerChoice){
     //prints the choices
-    console.log("You: " + playerChoice + " Computer: " + computerChoice)
+    choiceMessage.innerHTML = ("YOU CHOSE: " + playerChoice + "<br>COMPUTER CHOSE: " + computerChoice)
 
     //checks who won the game
     if(playerChoice === computerChoice){
@@ -40,52 +62,31 @@ function playRound(playerChoice, computerChoice){
     //returns the winner and changes the player count
     if(result === "win")
     {
-        console.log("You Win!");
+        winMessage.innerHTML = "YOU WON THE ROUND!"
         playerCount = playerCount + 1;
     }
     else if(result === "lose")
     {
-        console.log("You lose :(");
+        winMessage.innerHTML = "You lost that round :("
         computerCount = computerCount +1;
     }
     else if(result === "draw"){
-        console.log("Its a draw!");
+        winMessage.innerHTML = "its a draw :|"
     }
 
     //prints the scores
-    console.log("Score: Player: " + playerCount + " Computer: " + computerCount);
+    playerCounter.innerHTML = playerCount;
+    computerCounter.innerHTML = computerCount;
+
+    //responds based on who wins
+    if(playerCount === 3){
+        console.log("CONGRATS YOU WON!!")
+    }
+    else if(computerCount === 3)
+    {
+        console.log("you lost lol hahah")
+    }
 
 }   
-//Main game function
-function game(){
 
-        console.log("Enter rock paper or scissors to start the game :)")
-
-        //Creates a loop until the player or computer gets to 3 points
-        while(playerCount < 3 && computerCount < 3)
-        {
-            //plays a round with the computer choice function and the input from the user
-            playRound(prompt("What is your selection").toUpperCase(), getComputerChoice());
-        }
-
-        //responds based on who wins
-        if(playerCount === 3){
-            console.log("CONGRATS YOU WON!!")
-        }
-        else
-        {
-            console.log("you lost lol hahah")
-        }
-
-        //prompt for if you would like to go again
-        if(prompt("Would you like to go again?").toUpperCase()==="YES")
-        {
-            playerCount = 0
-            computerCount = 0
-            game()
-        }
-        
-}
-
-game()
 
